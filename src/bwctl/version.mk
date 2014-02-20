51d0916a11f798fc8475492364f6e4df2cb611cf
Philip Papadopoulos <ppapadopoulos@ucsd.edu>
2014-02-20 15:56:13 -0800

    bwctl 1.5.1

---
+++ src/bwctl/version.mk
@@ -0,0 +1,8 @@
+PKGROOT		= /opt/bwctl
+NAME        	= bwctl
+VERSION     	= 1.5.1
+RELEASE 	= 1
+TARBALL_POSTFIX	= tar.gz
+FILELIST 	= filelist
+RPM.SCRIPTLETS.FILE=scriptlets
+RPM.FILESLIST = $(FILELIST)
