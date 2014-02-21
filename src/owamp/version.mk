c199c21837779b97445f2de3ec6c42366b0dd755
Philip Papadopoulos <ppapadopoulos@ucsd.edu>
2014-02-20 16:14:39 -0800

    Adding owamp

---
+++ src/owamp/version.mk
@@ -0,0 +1,8 @@
+PKGROOT		= /opt/owamp
+NAME        	= owamp
+VERSION     	= 3.3
+RELEASE 	= 1
+TARBALL_POSTFIX	= tar.gz
+FILELIST 	= filelist
+RPM.SCRIPTLETS.FILE=scriptlets
+RPM.FILESLIST = $(FILELIST)
