09c4eb2b3aced12f5af66cd8c74e9f8c47706a1f
Philip Papadopoulos <ppapadopoulos@ucsd.edu>
2014-03-03 15:26:46 -0800

    Moved support around. created a basic wrapper around the perfSONAR supplied spec files

---
+++ src/perfSONAR/Minor_Packages/version.mk
@@ -0,0 +1,16 @@
+NAME        	= perfSONAR
+VERSION     	= 3.3.2
+RELEASE 	= 1
+COMPONENTS	= perl-Net-IPTrie \
+		perl-Math-Int64 \
+		iperf3 \
+		perl-Sys-Syslog \
+		perl-Net-Ping-External \
+		perl-Class-Fields \
+		tcptrace \
+		perl-Data-Validate-Domain \
+		perl-CGI-Ajax \
+		perl-Data-Validate-IP \
+		perl-RPC-XML \
+		perl-PlRPC \
+		xplot-tcptrace 
--- src/ps-support/multi-ethtool-0.1/version.mk
+++
@@ -1,4 +0,0 @@
-NAME=multi-ethtool
-VERSION=0.0
-BUILDROOT=$(CURDIR)/$(NAME).buildroot
-SOURCES = $(NAME).init $(NAME).sysconfig README BUGS COPYING
